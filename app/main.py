11f250aaec6a5ec7543cb4bd3e0a2a15fc7a021b
Zeljko Bozic <zeljko.zbozic@gmail.com>
2025-08-11 15:17:56 +0200

    Added cors middleware for angular app.

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 
 from app.api.routes import router
 from app.core.kafka_worker import start_kafka_consumer
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -16,4 +17,19 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+
+# Allow requests from Angular app
+origins = [
+    "http://localhost:4200",  # Angular dev server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # origins allowed to access
+    allow_credentials=True,
+    allow_methods=["*"],              # allow all HTTP methods
+    allow_headers=["*"],              # allow all headers
+)
+
 app.include_router(router)
